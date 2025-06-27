b2da700b53b218633e0f2409fa4c1b477d9ac892
akramou ouissal <kimwissalarmy@gmail.com>
2025-06-27 12:20:38 +0100

    Importation des modules nécessaires et chargement des variables d'environnement

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, session, send_from_directory, flash, url_for, current_app
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta, datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import os
+import uuid
+import sqlite3
+import requests
+from werkzeug.utils import secure_filename
+from PIL import Image
+import re
+from collections import Counter
+from pdf2image import convert_from_path
+import pytesseract
+from dotenv import load_dotenv 
+load_dotenv()
