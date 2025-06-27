0f50083bb0d216325c760734471e418a7ac6847f
Hasna Amal <hasna0amal@gmail.com>
2025-06-27 21:38:57 +0100

    import jsonify

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, send_from_directory, flash, url_for, current_app
+from flask import Flask, render_template, request, redirect, session, send_from_directory, flash, url_for, current_app, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta, datetime
 from sendgrid import SendGridAPIClient
