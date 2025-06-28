99af4d0b98c55108db7a1f30e951fa0f3ebed871
Hasna Amal <hasna0amal@gmail.com>
2025-06-28 20:57:05 +0100

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -281,15 +281,15 @@ document.querySelectorAll('.menu-button, .action-btn, .widget, .reminder-item, .
         }, 150);
       });
     });
-    function togglePassword(icon) {
-    const input = document.getElementById("password");
-    if (input.type === "password") {
-      input.type = "text";
-      icon.classList.remove("fa-eye");
-      icon.classList.add("fa-eye-slash");
-    } else {
-      input.type = "password";
-      icon.classList.remove("fa-eye-slash");
-      icon.classList.add("fa-eye");
-    }
-  }
+     document.querySelectorAll('.toggle-password').forEach(icon => {
+    icon.addEventListener('click', () => {
+      const input = icon.previousElementSibling;
+      if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.replace('fa-eye', 'fa-eye-slash');
+      } else {
+        input.type = 'password';
+        icon.classList.replace('fa-eye-slash', 'fa-eye');
+      }
+    });
+  });
