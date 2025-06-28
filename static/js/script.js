e2d3957ec4ec7a5bcfcbee8e6a3eb5a8f6ba6c8a
Hasna Amal <hasna0amal@gmail.com>
2025-06-28 20:46:36 +0100

    see/hide password js

--- static/js/script.js
+++ static/js/script.js
@@ -281,4 +281,15 @@ document.querySelectorAll('.menu-button, .action-btn, .widget, .reminder-item, .
         }, 150);
       });
     });
-    
+    function togglePassword(icon) {
+    const input = document.getElementById("password");
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
+  }
