0ba3bbbf307b34d9614d96006eff3490db101fb8
Hasna Amal <hasna0amal@gmail.com>
2025-06-28 23:25:30 +0100

    Update translator.js

--- static/js/translator.js
+++ static/js/translator.js
@@ -1,21 +1,30 @@
-
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
-        pageLanguage: 'fr',
-        includedLanguages: 'en,fr,ar',
-        autoDisplay: false
+      pageLanguage: 'fr',
+      includedLanguages: 'en,fr,ar',
+      autoDisplay: false
     }, 'google_translate_element');
-}
+  }
 
-function changeLanguage(selectElement) {
-    const lang = selectElement.value;
+  function changeLanguage(select) {
+    const lang = select.value;
     const interval = setInterval(() => {
-        const googleSelect = document.querySelector(".goog-te-combo");
-        if (googleSelect) {
-            googleSelect.value = lang;
-            googleSelect.dispatchEvent(new Event("change"));
-            clearInterval(interval);
-        }
+      const googleSelect = document.querySelector('.goog-te-combo');
+      if (googleSelect) {
+        googleSelect.value = lang;
+        googleSelect.dispatchEvent(new Event('change'));
+        clearInterval(interval);
+      }
     }, 500);
-}
+  }
 
+  function getCurrentLangFromCookie() {
+    const match = document.cookie.match(/(?:^|;\s*)googtrans=\/\w+\/(\w+)/);
+    return match ? match[1] : 'fr';
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const currentLang = getCurrentLangFromCookie();
+    const select = document.getElementById("selectLang");
+    if (select) select.value = currentLang;
+  });
