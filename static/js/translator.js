040d3f1f854f7f3ef5a24d9c6e34bf5113e378e5
Hasna Amal <hasna0amal@gmail.com>
2025-06-29 02:50:46 +0100

    Update translator.js

--- static/js/translator.js
+++ static/js/translator.js
@@ -7,16 +7,25 @@ function googleTranslateElementInit() {
   }
 
   function changeLanguage(select) {
-    const lang = select.value;
-    const interval = setInterval(() => {
-      const googleSelect = document.querySelector('.goog-te-combo');
-      if (googleSelect) {
-        googleSelect.value = lang;
-        googleSelect.dispatchEvent(new Event('change'));
-        clearInterval(interval);
-      }
-    }, 500);
+  const lang = select.value;
+
+  // Si la langue est arabe, modifier manuellement le titre
+  const title = document.getElementById('login-title');
+  if (title) {
+    title.textContent = (lang === 'ar') ? 'تسجيل الدخول' : 'Connexion';
   }
+
+  // Traduction avec Google Translate
+  const interval = setInterval(() => {
+    const googleSelect = document.querySelector(".goog-te-combo");
+    if (googleSelect) {
+      googleSelect.value = lang;
+      googleSelect.dispatchEvent(new Event("change"));
+      clearInterval(interval);
+    }
+  }, 500);
+}
+
 
   function getCurrentLangFromCookie() {
     const match = document.cookie.match(/(?:^|;\s*)googtrans=\/\w+\/(\w+)/);
